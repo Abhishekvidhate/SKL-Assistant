eb4db6715116d830b7677ae332d0f364e6211492
Abhishekvidhate <abhishekvidhate.dev@gmail.com>
2024-06-13 20:25:57 +0530

    UI updates

--- app.py
+++ app.py
@@ -13,7 +13,8 @@ st.sidebar.write("""
 This assistant helps you solve your Scikit-Learn doubts by leveraging a RAG-based LLM.
 - Developed by Abhishek Vidhate
 """)
-st.sidebar.write("[GitHub](https://github.com/Abhishekvidhate) | [LinkedIn](https://www.linkedin.com/in/abhishek-vidhate-21smdb/)")
+st.sidebar.write(
+    "[GitHub](https://github.com/Abhishekvidhate) | [LinkedIn](https://www.linkedin.com/in/abhishek-vidhate-21smdb/)")
 
 # Title and logo
 st.title("Scikit-Learn Assistant")
@@ -22,8 +23,10 @@ st.title("Scikit-Learn Assistant")
 if 'messages' not in st.session_state:
     st.session_state.messages = []
 
+
 def add_message(sender, text):
     st.session_state.messages.append({"sender": sender, "text": text})
+
 
 # Loading state message
 with st.spinner('Connecting to DB, please wait. As this app uses free cloud resources, it may take some time...'):
@@ -32,46 +35,44 @@ with st.spinner('Connecting to DB, please wait. As this app uses free cloud reso
     def load_rag_chain():
         return create_rag_chain(markdown_path)
 
+
     rag_chain = load_rag_chain()
 
 # Greeting message
 if not st.session_state.messages:
     add_message("assistant", "Hi there! How can I help you with your Scikit-Learn doubt today?")
 
-# Custom CSS for transparent boxes
-st.markdown(
-    """
-    <style>
-    .user-box {
-        background-color: rgba(0, 123, 255, 0.1);
-        padding: 10px;
-        border-radius: 5px;
-        margin-bottom: 10px;
-    }
-    .assistant-box {
-        background-color: rgba(255, 255, 255, 0.1);
-        padding: 10px;
-        border-radius: 5px;
-        margin-bottom: 10px;
-        display: flex;
-        align-items: center;
-    }
-    .assistant-box img {
-        margin-right: 10px;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+# # Custom CSS for transparent boxes
+# st.markdown(
+#     """
+#     <style>
+#     .user-box {
+#         background-color: rgba(0, 123, 255, 0.1);
+#         padding: 10px;
+#         border-radius: 5px;
+#         margin-bottom: 10px;
+#     }
+#     .assistant-box {
+#         background-color: rgba(255, 255, 255, 0.1);
+#         padding: 10px;
+#         border-radius: 5px;
+#         margin-bottom: 10px;
+#         display: flex;
+#         align-items: center;
+#     }
+#     .assistant-box img {
+#         margin-right: 10px;
+#     }
+#     </style>
+#     """,
+#     unsafe_allow_html=True
+# )
 
 # Display chat messages
 for message in st.session_state.messages:
     if message["sender"] == "assistant":
         st.markdown(
             f"""
-            <div class="assistant-box">
-                <img src="{logo_path}" width="50">
-                <div>
                     <strong>Assistant:</strong> {message['text']}
                 </div>
             </div>
